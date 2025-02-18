884411de53b051b88b5ef3e666e265718c1a0107
KhoaHocMai113 <doduongdangkhoa1103@gmail.com>
2025-02-18 11:07:17 +0700

    Auth Page UI

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,17 @@
+"use server";
+
+export const signIn = async () => {
+  try {
+  } catch (error) {
+    console.log("Error: ", error);
+  }
+};
+
+export const signUp = async (userData: SignUpParams) => {
+  try {
+    // Create user account
+    console.log("userData: ", userData);
+  } catch (error) {
+    console.log("Error: ", error);
+  }
+};
